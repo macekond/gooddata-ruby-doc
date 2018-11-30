e39ed456c9e80abfc88408a93827220109be231e
Filip Kirschner <filip.kirschner@applifting.cz>
2018-11-30 13:16:32 +0100

    Update license link

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -21,7 +21,7 @@ class Footer extends React.Component {
   render() {
     return (
       <footer className="nav-footer" id="footer">
-        <section className="copyright">Copyright © 2007–2018 GoodData Corporation. All Rights Reserved. Code licensed under an <a href="https://github.com/gooddata/gooddata-react-components/blob/master/LICENSE">BSD License</a>.</section>
+        <section className="copyright">Copyright © 2007–2018 GoodData Corporation. All Rights Reserved. Code licensed under an <a href="https://github.com/gooddata/gooddata-ruby/blob/master/LICENSE">BSD License</a>.</section>
       </footer>
     );
   }
